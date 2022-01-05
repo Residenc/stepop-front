732d859d1e275a69890826e7763bf1b5faa0ec10
Gabriel0206 <84149222+Gabriel0206@users.noreply.github.com>
2022-01-05 17:33:11 -0600

    Se hace carrito en front- end en espera de que se realice el servicio

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -7,21 +7,21 @@ import { BehaviorSubject } from 'rxjs';
 export class CartService {
 
   public cartItemList : any =[]
-  public ListarProduct = new BehaviorSubject<any>([]);
+  public productList = new BehaviorSubject<any>([]);
   public search = new BehaviorSubject<string>("");
 
   constructor() { }
   getProducts(){
-    return this.ListarProduct.asObservable();
+    return this.productList.asObservable();
   }
 
   setProduct(product : any){
     this.cartItemList.push(...product);
-    this.ListarProduct.next(product);
+    this.productList.next(product);
   }
   addtoCart(product : any){
     this.cartItemList.push(product);
-    this.ListarProduct.next(this.cartItemList);
+    this.productList.next(this.cartItemList);
     this.getTotalPrice();
     console.log(this.cartItemList)
   }
@@ -32,20 +32,17 @@ export class CartService {
     })
     return grandTotal;
   }
-
   removeCartItem(product: any){
     this.cartItemList.map((a:any, index:any)=>{
-      if(product.id== a.id){
+      if(product.id=== a.id){
         this.cartItemList.splice(index,1);
       }
     })
-    this.ListarProduct.next(this.cartItemList);
+    this.productList.next(this.cartItemList);
   }
-  
   removeAllCart(){
     this.cartItemList = []
-    this.ListarProduct.next(this.cartItemList);
+    this.productList.next(this.cartItemList);
   }
-
 
 }
--- src/app/views/main/products/general-view/general-view.component.ts
+++ src/app/views/main/products/general-view/general-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/cart/api.service';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { LoadScriptsService } from 'src/app/services/load-scripts/load-scripts.service';
 import { Producto, ProductosService } from 'src/app/services/productos/productos.service';
@@ -10,30 +11,39 @@ import { CartComponent } from 'src/app/views/header/bs_components/offcanvas/cart
   styleUrls: ['./general-view.component.scss']
 })
 export class GeneralViewComponent implements OnInit {
- 
-  ListarProducto:Producto []|any;
- 
-  constructor(private cartService:CartService ,private loadScripts:LoadScriptsService, public productoService:ProductosService, private router:Router) { 
-    loadScripts.loadS(["tooltip"]);
-    loadScripts.loadS(["scrollAOS"]);
-  }
+  public productList : any ;
+  public filterCategory : any
+  searchKey:string ="";
+  constructor(private api : ApiService, private cartService : CartService) { }
 
   ngOnInit(): void {
-    this.listarProduc();
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+      this.filterCategory = res;
+      this.productList.forEach((a:any) => {
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category ="fashion"
+        }
+        Object.assign(a,{quantity:1,total:a.price});
+      });
+      console.log(this.productList)
+    });
+
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
+    })
   }
   addtocart(item: any){
     this.cartService.addtoCart(item);
   }
-  listarProduc(){
-    this.productoService.getlistarproyser().subscribe(
-      res=>{
-
-        console.log(res);
-        this.ListarProducto=<any>res;
-      },
-      err=> console.log(err)
-    )
+  filter(category:string){
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category==''){
+        return a;
+      }
+    })
   }
-
 
 }
